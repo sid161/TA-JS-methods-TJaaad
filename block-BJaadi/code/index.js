6677554fc16c4af57ce69dc4026b7e72f4f5b2d6
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-01-08 15:19:39 +0530

    Assignment1|Block3

--- block-BJaadi/code/index.js
+++ block-BJaadi/code/index.js
@@ -3,18 +3,26 @@ var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6, 9];
 var strings = ['This', 'is', 'a', 'collection', 'of', 'words'];
 
 // - Find the index of `101` in numbers
+numbers.findIndex(101);
 
 // - Find the last index of `9` in numbers
+numbers.lastIndexOf(9);
 
 // - Convert value of strings array into a sentance like "This is a collection of words"
+strings.join("");
 
 // - Add two new words in the strings array "called" and "sentance"
+strings.push("called");
+strings.push("sentance");
 
 // - Again convert the updated array (strings) into sentance like "This is a collection of words called sentance"
+strings.join("");
 
 // - Remove the first word in the array (strings)
+strings.shift();
 
 // - Find all the words that contain 'is' use string method 'includes'
+strings.includes('is');
 
 // - Find all the words that contain 'is' use string method 'indexOf'
 
